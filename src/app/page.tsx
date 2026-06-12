8d1add1be0926d654c79b0e7ea74ef552afc4fb0
megatricycle <megatricycle@gmail.com>
2026-06-12 18:54:03 +0800

    Fix channel avatars not loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import dayjs from 'dayjs';
 import { google } from 'googleapis';
 import { Metadata } from 'next';
 import { unstable_noStore as noStore } from 'next/cache';
-import Image from 'next/image';
 import styles from './page.module.css';
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
@@ -12,21 +11,34 @@ const youtubeClient = google.youtube({
     version: 'v3',
 });
 
-async function fetchTotalSubscriberCountFromYouTube() {
+const CHANNELS = [
+    'UCMGfV7TVTmHhEErVJg1oHBQ',
+    'UCWQtYtq9EOB4-I5P-3fh8lA',
+    'UCtyWhCj3AqKh2dXctLkDtng',
+    'UCdXAk5MpyLD8594lm_OvtGQ',
+    'UC1iA6_NT4mtAcIII6ygrvCw',
+];
+
+async function fetchChannels() {
     const list = await youtubeClient.channels.list({
-        part: ['statistics'],
-        id: CHANNELS.map((channel) => channel.id),
+        part: ['snippet', 'statistics'],
+        id: CHANNELS,
         key: YOUTUBE_API_KEY,
     });
 
-    const total =
-        list.data.items?.reduce(
-            (total, current) =>
-                total + parseInt(current.statistics?.subscriberCount || '0'),
-            0
-        ) ?? 0;
+    return CHANNELS.map((channelId) => {
+        const channel = list.data.items?.find((item) => item.id === channelId);
 
-    return total;
+        return {
+            id: channel?.id || '',
+            name: channel?.snippet?.title || '',
+            avatar: channel?.snippet?.thumbnails?.high?.url || '',
+            url: `https://www.youtube.com/channel/${channelId}`,
+            subscribersCount: parseInt(
+                channel?.statistics?.subscriberCount || '0',
+            ),
+        };
+    });
 }
 
 async function fetchTotalSubscriberCountFromCache() {
@@ -54,7 +66,9 @@ async function fetchTotalSubscriberCount() {
     const countFromCache = await fetchTotalSubscriberCountFromCache();
 
     if (!countFromCache) {
-        const countFromYouTube = await fetchTotalSubscriberCountFromYouTube();
+        const channels = await fetchChannels();
+
+        const countFromYouTube = getTotalSubscribersCount(channels);
 
         await updateCache(countFromYouTube);
 
@@ -63,39 +77,6 @@ async function fetchTotalSubscriberCount() {
 
     return countFromCache;
 }
-
-const CHANNELS = [
-    {
-        id: 'UCMGfV7TVTmHhEErVJg1oHBQ',
-        url: 'https://www.youtube.com/@HiodoshiAo',
-        name: 'Hiodoshi Ao',
-        avatar: 'https://yt3.googleusercontent.com/-D2Cf4dIQGO_CcY_1F9i63TcHyY0EuPV1pYskJQmQHIClJGOt34BoI84zlgje_THCw8AprB6=s176-c-k-c0x00ffffff-no-rj',
-    },
-    {
-        id: 'UCWQtYtq9EOB4-I5P-3fh8lA',
-        url: 'https://www.youtube.com/@OtonoseKanade',
-        name: 'Otonose Kanade',
-        avatar: 'https://yt3.googleusercontent.com/o03i3rWw98BSquRZFhyiDQuunr1cr_9xEBVNNx3Cq8vqlJZVKXMgKsVLGW2AlbsFTvphGiHRCg0=s176-c-k-c0x00ffffff-no-rj',
-    },
-    {
-        id: 'UCtyWhCj3AqKh2dXctLkDtng',
-        url: 'https://www.youtube.com/@IchijouRirika',
-        name: 'Ichijou Ririka',
-        avatar: 'https://yt3.googleusercontent.com/TQwdYxMCQYmBQskSxmdAbfAqRR__ROlB-mFGlCFqLF4C-6vHpjYkWj9GbnlKOoOTaOMssRGw=s176-c-k-c0x00ffffff-no-rj',
-    },
-    {
-        id: 'UCdXAk5MpyLD8594lm_OvtGQ',
-        url: 'https://www.youtube.com/@JuufuuteiRaden',
-        name: 'Juufuutei Raden',
-        avatar: 'https://yt3.googleusercontent.com/MrOx47-A0RkLxHN5Wh8stc3SYfbPGNHdJY9AnjD5mRkuKYVeYjxlBSnzKHtqTjDQ3Lm_MRCjcA=s176-c-k-c0x00ffffff-no-rj',
-    },
-    {
-        id: 'UC1iA6_NT4mtAcIII6ygrvCw',
-        url: 'https://www.youtube.com/@TodorokiHajime',
-        name: 'Todoroki Hajime',
-        avatar: 'https://yt3.googleusercontent.com/vMM_SKbkipyDVJkUYPPWlQkgThE1rXMSh7hkhqvC_Qs-iTigfyKW23OfLH5U1HFTZIcsHR2Z=s176-c-k-c0x00ffffff-no-rj',
-    },
-];
 
 function ProgressBar({ percentage }: { percentage: number }) {
     return (
@@ -117,13 +98,13 @@ export async function generateMetadata(): Promise<Metadata> {
     const totalSubscribersCount = await fetchTotalSubscriberCount();
     const percentage = Math.min(
         Math.floor((totalSubscribersCount / GOAL_COUNT) * 100),
-        100
+        100,
     );
 
     const description =
         percentage < 100
             ? `ReGLOSS has reached ${percentage.toFixed(
-                  0
+                  0,
               )}% of their goal. Current subscribers: ${totalSubscribersCount.toLocaleString()}`
             : 'ReGLOSS has reached the goal for their 3D debut. Congratulations!';
 
@@ -135,11 +116,23 @@ export async function generateMetadata(): Promise<Metadata> {
     };
 }
 
+function getTotalSubscribersCount(
+    channels: Awaited<ReturnType<typeof fetchChannels>>,
+) {
+    return channels.reduce(
+        (total, channel) => total + channel.subscribersCount,
+        0,
+    );
+}
+
 export default async function Home() {
-    const totalSubscribersCount = await fetchTotalSubscriberCount();
+    const channels = await fetchChannels();
+
+    const totalSubscribersCount = getTotalSubscribersCount(channels);
+
     const percentage = Math.min(
         Math.floor((totalSubscribersCount / GOAL_COUNT) * 100),
-        100
+        100,
     );
 
     const text =
@@ -168,10 +161,11 @@ export default async function Home() {
                     <p>Subscribers Goal: {GOAL_COUNT.toLocaleString()}</p>
                 </div>
                 <div className={styles['channels-container']}>
-                    {CHANNELS.map((channel) => (
+                    {channels.map((channel) => (
                         <a key={channel.id} href={channel.url} target="_blank">
                             <div className={styles['channel-container']}>
-                                <Image
+                                {/* eslint-disable-next-line @next/next/no-img-element */}
+                                <img
                                     className={styles['channel-avatar']}
                                     src={channel.avatar}
                                     alt={channel.name}
